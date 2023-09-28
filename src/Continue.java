9db7203ab7180a94fea6504e42f77a740c5a95cb
FajarPike <fpikekah@gmail.com>
2023-09-28 15:53:39 +0700

    merubah continue

--- src/Continue.java
+++ src/Continue.java
@@ -2,7 +2,7 @@ public class Continue {
   public static void main(String[] args) {
 
 
-    for(var counter = 1; counter <= 100; counter++){
+    for(int counter = 1; counter <= 100; counter++){
       if(counter % 2 == 0){
         continue;
       }
